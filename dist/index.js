8994a32e82530081ac54449591c827d5f4b4795d
Igor Pnev <tkospb@gmail.com>
2017-04-17 21:38:29 +0300

    Add props-type dependence and remove React.PropTypes

--- dist/index.js
+++ dist/index.js
@@ -1,83 +1,87 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports.default = HamburgerMenu;
 
-var _react = require('react');
+var _propTypes = require("prop-types");
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var objectAssign = require('object-assign');
+var objectAssign = require("object-assign");
 
 function HamburgerMenu(props) {
-	var width = (props.width || 36) + 'px',
-	    height = (props.height || 30) + 'px',
-	    halfHeight = parseInt(height.replace('px', '')) / 2 + 'px',
-	    isOpen = props.isOpen || false,
-	    strokeWidth = props.strokeWidth || 2,
-	    halfStrokeWidth = '-' + strokeWidth / 2 + 'px',
-	    animationDuration = props.animationDuration || '0.4';
+  var width = (props.width || 36) + "px",
+      height = (props.height || 30) + "px",
+      halfHeight = parseInt(height.replace("px", "")) / 2 + "px",
+      isOpen = props.isOpen || false,
+      strokeWidth = props.strokeWidth || 2,
+      halfStrokeWidth = "-" + strokeWidth / 2 + "px",
+      animationDuration = props.animationDuration || "0.4";
 
-	var getTransformValue = function getTransformValue(isOpen, defaultPos, rotateVal) {
-		return 'translate3d(0,' + (isOpen ? halfHeight : defaultPos) + ',0) rotate(' + (isOpen ? rotateVal + 'deg' : '0') + ')';
-	};
+  var getTransformValue = function getTransformValue(isOpen, defaultPos, rotateVal) {
+    return "translate3d(0," + (isOpen ? halfHeight : defaultPos) + ",0) rotate(" + (isOpen ? rotateVal + "deg" : "0") + ")";
+  };
 
-	var styles = {
-		container: {
-			width: width,
-			height: height,
-			position: 'relative',
-			transform: 'rotate(' + (props.rotate || 0) + 'deg)'
-		},
-		lineBase: {
-			display: 'block',
-			height: strokeWidth + 'px',
-			width: '100%',
-			background: props.color || '#000',
-			transitionTimingFunction: 'ease',
-			transitionDuration: animationDuration + 's',
-			borderRadius: (props.borderRadius || 0) + 'px',
-			transformOrigin: 'center',
-			position: 'absolute'
-		},
-		firstLine: {
-			transform: getTransformValue(isOpen, 0, 45),
-			marginTop: halfStrokeWidth
-		},
-		secondLine: {
-			transitionTimingFunction: 'ease-out',
-			transitionDuration: animationDuration / 4 + 's',
-			opacity: isOpen ? '0' : '1',
-			top: halfHeight,
-			marginTop: halfStrokeWidth
-		},
-		thirdLine: {
-			transform: getTransformValue(isOpen, height, -45),
-			marginTop: halfStrokeWidth
-		}
-	};
+  var styles = {
+    container: {
+      width: width,
+      height: height,
+      position: "relative",
+      transform: "rotate(" + (props.rotate || 0) + "deg)"
+    },
+    lineBase: {
+      display: "block",
+      height: strokeWidth + "px",
+      width: "100%",
+      background: props.color || "#000",
+      transitionTimingFunction: "ease",
+      transitionDuration: animationDuration + "s",
+      borderRadius: (props.borderRadius || 0) + "px",
+      transformOrigin: "center",
+      position: "absolute"
+    },
+    firstLine: {
+      transform: getTransformValue(isOpen, 0, 45),
+      marginTop: halfStrokeWidth
+    },
+    secondLine: {
+      transitionTimingFunction: "ease-out",
+      transitionDuration: animationDuration / 4 + "s",
+      opacity: isOpen ? "0" : "1",
+      top: halfHeight,
+      marginTop: halfStrokeWidth
+    },
+    thirdLine: {
+      transform: getTransformValue(isOpen, height, -45),
+      marginTop: halfStrokeWidth
+    }
+  };
 
-	return _react2.default.createElement(
-		'div',
-		{ style: styles.container, onClick: props.menuClicked },
-		_react2.default.createElement('span', { style: objectAssign({}, styles.lineBase, styles.firstLine) }),
-		_react2.default.createElement('span', { style: objectAssign({}, styles.lineBase, styles.secondLine) }),
-		_react2.default.createElement('span', { style: objectAssign({}, styles.lineBase, styles.thirdLine) })
-	);
-};
+  return _react2.default.createElement(
+    "div",
+    { style: styles.container, onClick: props.menuClicked },
+    _react2.default.createElement("span", { style: objectAssign({}, styles.lineBase, styles.firstLine) }),
+    _react2.default.createElement("span", { style: objectAssign({}, styles.lineBase, styles.secondLine) }),
+    _react2.default.createElement("span", { style: objectAssign({}, styles.lineBase, styles.thirdLine) })
+  );
+}
 
 HamburgerMenu.PropTypes = {
-	isOpen: _react2.default.PropTypes.bool.isRequired,
-	menuClicked: _react2.default.PropTypes.func.isRequired,
-	width: _react2.default.PropTypes.number,
-	height: _react2.default.PropTypes.number,
-	strokeWidth: _react2.default.PropTypes.number,
-	rotate: _react2.default.PropTypes.number,
-	color: _react2.default.PropTypes.string,
-	borderRadius: _react2.default.PropTypes.number,
-	animationDuration: _react2.default.PropTypes.number
+  isOpen: _propTypes2.default.bool.isRequired,
+  menuClicked: _propTypes2.default.func.isRequired,
+  width: _propTypes2.default.number,
+  height: _propTypes2.default.number,
+  strokeWidth: _propTypes2.default.number,
+  rotate: _propTypes2.default.number,
+  color: _propTypes2.default.string,
+  borderRadius: _propTypes2.default.number,
+  animationDuration: _propTypes2.default.number
 };
--- src/hamburger-menu.jsx
+++ src/hamburger-menu.jsx
@@ -1,72 +1,72 @@
-import React from 'react';
-var objectAssign = require('object-assign');
+import PropTypes from "prop-types";
+import React from "react";
 
+var objectAssign = require("object-assign");
 
 export default function HamburgerMenu(props) {
-	const width       = `${props.width || 36}px`,
-	height            = `${props.height || 30}px`,
-	halfHeight        = `${parseInt(height.replace('px', '')) / 2}px`,
-	isOpen            = props.isOpen || false,
-	strokeWidth       = props.strokeWidth || 2,
-    halfStrokeWidth   = `-${strokeWidth / 2}px`,
-    animationDuration = props.animationDuration || '0.4';
+  const width = `${props.width || 36}px`,
+    height = `${props.height || 30}px`,
+    halfHeight = `${parseInt(height.replace("px", "")) / 2}px`,
+    isOpen = props.isOpen || false,
+    strokeWidth = props.strokeWidth || 2,
+    halfStrokeWidth = `-${strokeWidth / 2}px`,
+    animationDuration = props.animationDuration || "0.4";
 
-	const getTransformValue = (isOpen, defaultPos, rotateVal) => (
-		`translate3d(0,${isOpen ? halfHeight : defaultPos},0) rotate(${isOpen ? `${rotateVal}deg` : '0'})`
-	);
+  const getTransformValue = (isOpen, defaultPos, rotateVal) =>
+    `translate3d(0,${isOpen ? halfHeight : defaultPos},0) rotate(${isOpen ? `${rotateVal}deg` : "0"})`;
 
-	const styles = {
-		container: {
-			width,
-			height,
-			position: 'relative',
-			transform: `rotate(${props.rotate || 0}deg)`
-		},
-		lineBase: {
-			display: 'block',
-			height: `${strokeWidth}px`,
-			width: '100%',
-			background: props.color || '#000',
-			transitionTimingFunction: 'ease',
-			transitionDuration : `${animationDuration}s`,
-			borderRadius: `${props.borderRadius || 0}px`,
-			transformOrigin: 'center',
-			position: 'absolute'
-		},
-		firstLine: {
-			transform: getTransformValue(isOpen, 0, 45),
-			marginTop: halfStrokeWidth,
-		},
-		secondLine: {
-      transitionTimingFunction: 'ease-out',
-			transitionDuration : `${animationDuration / 4}s`,
-			opacity: isOpen ? '0' : '1',
-			top: halfHeight,
-			marginTop: halfStrokeWidth
-		},
-		thirdLine: {
-			transform: getTransformValue(isOpen, height, -45),
-			marginTop: halfStrokeWidth
-		}
-	};
+  const styles = {
+    container: {
+      width,
+      height,
+      position: "relative",
+      transform: `rotate(${props.rotate || 0}deg)`
+    },
+    lineBase: {
+      display: "block",
+      height: `${strokeWidth}px`,
+      width: "100%",
+      background: props.color || "#000",
+      transitionTimingFunction: "ease",
+      transitionDuration: `${animationDuration}s`,
+      borderRadius: `${props.borderRadius || 0}px`,
+      transformOrigin: "center",
+      position: "absolute"
+    },
+    firstLine: {
+      transform: getTransformValue(isOpen, 0, 45),
+      marginTop: halfStrokeWidth
+    },
+    secondLine: {
+      transitionTimingFunction: "ease-out",
+      transitionDuration: `${animationDuration / 4}s`,
+      opacity: isOpen ? "0" : "1",
+      top: halfHeight,
+      marginTop: halfStrokeWidth
+    },
+    thirdLine: {
+      transform: getTransformValue(isOpen, height, -45),
+      marginTop: halfStrokeWidth
+    }
+  };
 
-	return (
-		<div style={styles.container} onClick={props.menuClicked}>
-		  <span style={objectAssign({}, styles.lineBase, styles.firstLine)}></span>
-		  <span style={objectAssign({}, styles.lineBase, styles.secondLine)}></span>
-		  <span style={objectAssign({}, styles.lineBase, styles.thirdLine)}></span>
-		</div>
-	);
-};
+  return (
+    <div style={styles.container} onClick={props.menuClicked}>
+      <span style={objectAssign({}, styles.lineBase, styles.firstLine)} />
+      <span style={objectAssign({}, styles.lineBase, styles.secondLine)} />
+      <span style={objectAssign({}, styles.lineBase, styles.thirdLine)} />
+    </div>
+  );
+}
 
 HamburgerMenu.PropTypes = {
-	isOpen: React.PropTypes.bool.isRequired,
-	menuClicked: React.PropTypes.func.isRequired,
-	width: React.PropTypes.number,
-	height: React.PropTypes.number,
-	strokeWidth: React.PropTypes.number,
-	rotate: React.PropTypes.number,
-	color: React.PropTypes.string,
-	borderRadius: React.PropTypes.number,
-	animationDuration: React.PropTypes.number
+  isOpen: PropTypes.bool.isRequired,
+  menuClicked: PropTypes.func.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  strokeWidth: PropTypes.number,
+  rotate: PropTypes.number,
+  color: PropTypes.string,
+  borderRadius: PropTypes.number,
+  animationDuration: PropTypes.number
 };

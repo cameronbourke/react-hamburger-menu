92af5126282c2fcaa7aa89fa53404daa0229abee
gcosgreave <gcosgreave@me.com>
2019-07-31 13:29:06 +0100

    Add zindex prop

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-React Hamburger Menu
-=========================
+# React Hamburger Menu
 
 Built for React, this is a handy UI component for a menu icon in a mobile site or application.
 
@@ -43,15 +42,16 @@ An example use of React Hamburger Menu looks like:
 
 ```js
 <HamburgerMenu
-	isOpen={this.state.open}
-	menuClicked={this.handleClick.bind(this)}
-	width={18}
-	height={15}
-	strokeWidth={1}
-	rotate={0}
-	color='black'
-	borderRadius={0}
-	animationDuration={0.5}
+  isOpen={this.state.open}
+  menuClicked={this.handleClick.bind(this)}
+  width={18}
+  height={15}
+  strokeWidth={1}
+  rotate={0}
+  color="black"
+  borderRadius={0}
+  animationDuration={0.5}
+  zIndex={10}
 />
 ```
 
@@ -59,17 +59,20 @@ Note, not all props are required. All the props besides `isOpen` and `menuClicke
 
 ## Options
 
-Property  | Type | Default | Description
-------------- | ------------- | --------- | ----------
-isOpen            | bool   | undefined | determines whether the menu is a hamburger or cross icon
-menuClicked       | func   | undefined | will be invoked when the component is clicked
-width             | number | 36        | the width of the icon
-height            | number | 30        | the height of the icon
-strokeWidth       | number | 2         | the stroke width of the lines
-rotate            | number | 0         | the rotation of the icon, eg {45} would be `45deg`
-color             | string | #000      | the color of both icons
-borderRadius      | number | 0         | the border radius of the lines
-animationDuration | number | 0.4       | the length of time it takes for the icon transitions to complete.
+| Property          | Type   | Default   | Description                                              |
+| ----------------- | ------ | --------- | -------------------------------------------------------- |
+| isOpen            | bool   | undefined | determines whether the menu is a hamburger or cross icon |
+| menuClicked       | func   | undefined | will be invoked when the component is clicked            |
+| width             | number | 36        | the width of the icon                                    |
+| height            | number | 30        | the height of the icon                                   |
+| strokeWidth       | number | 2         | the stroke width of the lines                            |
+| rotate            | number | 0         | the rotation of the icon, eg {45} would be `45deg`       |
+| color             | string | #000      | the color of both icons                                  |
+| borderRadius      | number | 0         | the border radius of the lines                           |
+| animationDuration | number | 0.4       | the length of time it takes for the icon transitions     |
+
+to complete.
+zIndex | number | 0 | will change the stack order of the burger icon
 
 ## Todo
 
--- src/HamburgerMenu.js
+++ src/HamburgerMenu.js
@@ -2,70 +2,72 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function HamburgerMenu(props) {
-	const width       = `${props.width || 36}px`,
-	height            = `${props.height || 30}px`,
-	halfHeight        = `${parseInt(height.replace('px', '')) / 2}px`,
-	isOpen            = props.isOpen || false,
-	strokeWidth       = props.strokeWidth || 2,
-    halfStrokeWidth   = `-${strokeWidth / 2}px`,
+  const width = `${props.width || 36}px`,
+    height = `${props.height || 30}px`,
+    halfHeight = `${parseInt(height.replace('px', '')) / 2}px`,
+    isOpen = props.isOpen || false,
+    strokeWidth = props.strokeWidth || 2,
+    halfStrokeWidth = `-${strokeWidth / 2}px`,
     animationDuration = props.animationDuration || '0.4';
+  zIndex = props.zIndex || 0;
 
-	const getTransformValue = (isOpen, defaultPos, rotateVal) => (
-		`translate3d(0,${isOpen ? halfHeight : defaultPos},0) rotate(${isOpen ? `${rotateVal}deg` : '0'})`
-	);
+  const getTransformValue = (isOpen, defaultPos, rotateVal) =>
+    `translate3d(0,${isOpen ? halfHeight : defaultPos},0) rotate(${
+      isOpen ? `${rotateVal}deg` : '0'
+    })`;
 
-	const styles = {
-		container: {
-			width,
-			height,
-			position: 'relative',
-			transform: `rotate(${props.rotate || 0}deg)`
-		},
-		lineBase: {
-			display: 'block',
-			height: `${strokeWidth}px`,
-			width: '100%',
-			background: props.color || '#000',
-			transitionTimingFunction: 'ease',
-			transitionDuration : `${animationDuration}s`,
-			borderRadius: `${props.borderRadius || 0}px`,
-			transformOrigin: 'center',
-			position: 'absolute'
-		},
-		firstLine: {
-			transform: getTransformValue(isOpen, 0, 45),
-			marginTop: halfStrokeWidth,
-		},
-		secondLine: {
+  const styles = {
+    container: {
+      width,
+      height,
+      position: 'relative',
+      transform: `rotate(${props.rotate || 0}deg)`
+    },
+    lineBase: {
+      display: 'block',
+      height: `${strokeWidth}px`,
+      width: '100%',
+      background: props.color || '#000',
+      transitionTimingFunction: 'ease',
+      transitionDuration: `${animationDuration}s`,
+      borderRadius: `${props.borderRadius || 0}px`,
+      transformOrigin: 'center',
+      position: 'absolute'
+    },
+    firstLine: {
+      transform: getTransformValue(isOpen, 0, 45),
+      marginTop: halfStrokeWidth
+    },
+    secondLine: {
       transitionTimingFunction: 'ease-out',
-			transitionDuration : `${animationDuration / 4}s`,
-			opacity: isOpen ? '0' : '1',
-			top: halfHeight,
-			marginTop: halfStrokeWidth
-		},
-		thirdLine: {
-			transform: getTransformValue(isOpen, height, -45),
-			marginTop: halfStrokeWidth
-		}
-	};
+      transitionDuration: `${animationDuration / 4}s`,
+      opacity: isOpen ? '0' : '1',
+      top: halfHeight,
+      marginTop: halfStrokeWidth
+    },
+    thirdLine: {
+      transform: getTransformValue(isOpen, height, -45),
+      marginTop: halfStrokeWidth
+    }
+  };
 
-	return (
-		<div style={styles.container} onClick={props.menuClicked}>
-		  <span style={Object.assign({}, styles.lineBase, styles.firstLine)}></span>
-		  <span style={Object.assign({}, styles.lineBase, styles.secondLine)}></span>
-		  <span style={Object.assign({}, styles.lineBase, styles.thirdLine)}></span>
-		</div>
-	);
-};
+  return (
+    <div style={styles.container} onClick={props.menuClicked}>
+      <span style={Object.assign({}, styles.lineBase, styles.firstLine)} />
+      <span style={Object.assign({}, styles.lineBase, styles.secondLine)} />
+      <span style={Object.assign({}, styles.lineBase, styles.thirdLine)} />
+    </div>
+  );
+}
 
 HamburgerMenu.propTypes = {
-	isOpen: PropTypes.bool.isRequired,
-	menuClicked: PropTypes.func.isRequired,
-	width: PropTypes.number,
-	height: PropTypes.number,
-	strokeWidth: PropTypes.number,
-	rotate: PropTypes.number,
-	color: PropTypes.string,
-	borderRadius: PropTypes.number,
-	animationDuration: PropTypes.number
+  isOpen: PropTypes.bool.isRequired,
+  menuClicked: PropTypes.func.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  strokeWidth: PropTypes.number,
+  rotate: PropTypes.number,
+  color: PropTypes.string,
+  borderRadius: PropTypes.number,
+  animationDuration: PropTypes.number
 };
